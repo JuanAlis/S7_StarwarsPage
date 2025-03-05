dd9fc62b72b99c19d23dd4881a1207d4c794ab56
JuanAlis <juanalis00710@gmail.com>
2025-03-05 19:30:17 +0100

    Añadiendo config

--- starwars-page/vite.config.ts
+++ starwars-page/vite.config.ts
@@ -5,5 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  }
 });
 
